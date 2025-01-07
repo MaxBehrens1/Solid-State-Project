95aeb703d6a6e776a5091d32041e4ff9b427e4b5
Aarnav Panda <aarnavpanda11@gmail.com>
2025-01-07 12:30:38 +0100

    graphing done for drude temp invariance

--- Confirm_Drude_temp_distb.py
+++ Confirm_Drude_temp_distb.py
@@ -6,7 +6,8 @@ from scipy.constants import m_e, k
 import os
 import csv
 from pathlib import Path # this makes iterating easier
-COLLATE_DATA = True 
+import matplotlib.pyplot as plt
+COLLATE_DATA = False
 
 # Set the folder where you keep things
 data_folder = Path("C:/Users/arceu/OneDrive - The University of Manchester/Year 3/Estado Sólido/sss/Solid-State-Project/Data/Drude_current/")
@@ -36,30 +37,35 @@ def Analysis(filename):
     #    print(f"(Average,std) for data[{i}] is ({averages[i]:.4f}, {std[i]:.4f}).")
     #print("Data: ", file_name)
     
-    temp = filename.relative_to(data_folder)
-    name = str(temp)
+    parts = str(filename).split("_")
+    for part in parts:
+        if part.endswith("K"):
+            temperature = int(part[:-1])  # Remove the 'K' and convert to int
+            print(f"Temperature: {temperature} K")
+            break
     # DrudeTemp(abs(x_data), abs(y_data))
     
     #This is for the Drude current comparison:
-    return [name, abs(averages[1]), std[1]]
+    return [temperature, abs(averages[1]), std[1]]
     
     
     
 # Define the folder and output file
-output_name = input("Name the output file (xyz.csv) (CHECK EXISTING FILES SO AS TO NOT OVERWRITE): ")
-output_file = Path(output_name)
+
 
 Analysis(filename)
 
 # %% COLLATE DATA 
 
 if COLLATE_DATA:
+    output_name = input("Name the output file (xyz.csv) (CHECK EXISTING FILES SO AS TO NOT OVERWRITE): ")
+    output_file = Path(output_name)
     # Collect results for each file
     results = []
     for file in data_folder.iterdir():
         temp = file.relative_to(data_folder)
         print(str(temp))
-        if file.is_file() and file.suffix == '.txt':  # Filter for text files
+        if file.is_file() and str(file).endswith('vel.txt'):  # Filter for text files
             result = Analysis(file)
             if result:  # If valid result, add to list
                 results.append(result)
@@ -72,3 +78,18 @@ if COLLATE_DATA:
     
     print(f"Results compiled into {output_file}")
     
+data = np.genfromtxt("Drude_Current.csv", delimiter=',', skip_header=1)
+temps = data[:,0]
+vel = data[:,1] * 10000
+vel_errs =  data[:,2] * 10000
+
+plt.errorbar(temps, vel, vel_errs, ls = 'none', fmt = 'o', label ='Data', 
+             capsize=2, color = 'black')
+plt.title('Tau = 1ps, Drude')
+plt.xlabel('Temperature (K)')
+plt.ylabel('Drift vel (m/s)')
+plt.legend()
+plt.grid()
+plt.savefig("Drude_fixed_Tau_varying_T_drift_vel.png", dpi=400)
+plt.show()
+
