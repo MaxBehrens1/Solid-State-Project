29ed27b88e0f53acd6205074bc49784a5c5b617a
Max Behrens <max.behrens22@imperial.ac.uk>
2025-01-08 19:53:03 +0100

    Finished hall angle

--- Hall_Angle.py
+++ Hall_Angle.py
@@ -11,16 +11,20 @@ except:
 
 folder = str(pathlib.Path(__file__).parent.resolve()) + r'/Data/Hall_angle/'
 
-def mean_std(data):
-    interval = len(data)//5
-    means = []
+def mean_std(data_x, data_y):
+    interval = len(data_x)//5
+    angles = []
     for ind in range(5):
         if ind != 4:
-            mean_val = np.mean(data[ind*interval:(ind+1)*interval])
+            mean_x = np.mean(data_x[ind*interval:(ind+1)*interval])
+            mean_y = np.mean(data_y[ind*interval:(ind+1)*interval])
+            cur_angle = np.arctan(mean_y/mean_x)
         else:
-            mean_val = np.mean(data[ind*interval:])
-        means.append(mean_val)
-    return np.mean(means), np.std(means)
+            mean_x = np.mean(data_x[ind*interval:])
+            mean_y = np.mean(data_y[ind*interval:])
+            cur_angle = np.arctan(mean_y/mean_x)
+        angles.append(cur_angle)
+    return np.mean(angles), np.std(angles)
 
 file_list = os.listdir(folder)
 angle_d = []
@@ -31,16 +35,12 @@ for file in file_list:
     if file[0] == 's':
         time, x, y, Energy = ReadData(path, 'sommer')
         R_val = float(file[8:14])
-        average_x = np.mean(x)
-        average_y = np.mean(y)
-        angle = np.arctan(average_y/average_x)
-        angle_s.append([Btau,angle,R_val])
+        average_angle, error_angle = mean_std(x, y)
+        angle_s.append([Btau,average_angle,R_val, error_angle])
     else:
         times, _, y_data, x_data = ReadData(path, 'drude')
-        average_x = np.mean(x_data)
-        average_y = np.mean(y_data)
-        angle = np.arctan(average_y/average_x)
-        angle_d.append([Btau,angle])
+        average_angle, error_angle = mean_std(x_data, y_data)
+        angle_d.append([Btau,average_angle, error_angle])
 angle_d = np.array(angle_d)
 angle_s = np.array(angle_s)
 
@@ -55,16 +55,17 @@ for i, val in enumerate(R_list):
     cur_data = []
     for j in angle_s:
         if j[2] == val:
-            cur_data.append([j[0],j[1]])
+            cur_data.append([j[0],j[1], j[3]])
     cur_data = np.array(cur_data)
-    plt.errorbar(cur_data[:,0], cur_data[:,1], linestyle='',
+    plt.errorbar(cur_data[:,0], cur_data[:,1], cur_data[:,2], linestyle='',
              label =f'R={val:.2e}', fmt=symbols[i], ecolor=colors[i], capsize=2, markeredgecolor=colors[i],
                  markerfacecolor=colors[i], alpha = 0.5)
 
 
-
 plt.plot(x_val, arctangent(x_val), label = 'Theory')
-plt.plot(angle_d[:,0], angle_d[:,1], 'x', label = 'Drude')
+plt.errorbar(angle_d[:,0], angle_d[:,1], angle_d[:,2], linestyle='',
+             label =f'R={val:.2e}', fmt=symbols[-1], ecolor=colors[-1], capsize=2, markeredgecolor=colors[-1],
+                 markerfacecolor=colors[-1], alpha = 0.5)
 
 plt.legend()
 plt.title('Hall Angle')
